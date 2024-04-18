1c5b4480f9da1ed9839c3b9d7f97db442bba8633
rizqicy <dev@rizqi.cy>
2024-04-19 02:44:41 +0700

    add git plugings (fugitive, rhubarb)

--- init.lua
+++ init.lua
@@ -241,6 +241,9 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
+  'tpope/vim-fugitive', -- Git related plugin #1
+  'tpope/vim-rhubarb', -- Git related plugin #2
+
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
